6946c0b519413a4387f50c678676ad630d654e7d
jefin38 <147368514+jefin38@users.noreply.github.com>
2024-11-11 10:04:13 +0530

    Add files via upload

---
+++ E-Learning System/allfoot.php
@@ -0,0 +1,34 @@
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+               
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+
+
+    <div style="background: #000; padding: 5px; color: #fff; text-align: center;">
+       Copyright &copy; learnonline 2024 MCA project Made by Jefin Daniel</p>
+    </div>
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
---
+++ E-Learning System/index.php
@@ -0,0 +1,206 @@
+<?php include('allhead.php'); ?>
+
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+	<!-- Indicators --><br><br>
+	
+
+	<!-- Wrapper for slides -->
+	
+			
+			
+		</div>
+		<div class="item active"><br><br>
+			<div class="fill" style="background-image:url('images/1900x10802.png');"></div>
+			
+		</div>
+
+
+		
+        </div>
+      	
+</header>
+
+<!-- Page Content -->
+<div class="container" style="max-width: 1200px;">
+
+	<!-- Marketing Icons Section -->
+	<div class="row">
+		<div class="col-lg-12"><br><br><br><br>
+		<br><center>	<h1>	
+                    Welcome to LearnOnline </h1> 
+		<h3 style="font-family: 'Roboto', sans-serif;">Making Learning Easy</h3><br><br><br><br>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4></i> About Us</h4>
+				</div>
+				<div class="panel-body">
+				<br>
+						
+							<p> The “LearnOnline” Website (web based application) is useful for the
+							students, faculty, guest whomever likes to learn from web using E-Learn
+							(Videos), as well Check result, schedules of assessment and all that task like
+							event, news, students can find out list of fresh courses offered by them and
+							admission procedure, discussion forum..
+						</p>
+				
+					
+					</ul>
+						
+					<a href="about" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4></i> Objectives</h4>
+				</div>
+				<div class="panel-body">
+					<p>
+					<ul style="list-style-type:disc">
+						<li>Keep records of all Students, Faculty & Admin</li>
+						<li>To make the institute truly a paperless institute.</li>
+						<li>To learn from by watching videos (e-learn).
+						</li>
+						<li>To make the institute truly a Cloud Based Institute.
+						</li>
+						<li>For reducing manual work and mental conflict.<br></li>
+
+						</ul>
+					 </p>
+					<a href="about" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4></i> Project- Technology Used</h4>
+				</div>
+				<div class="panel-body">
+					<p>
+					<ul style="list-style-type:disc">
+					<li>DBMS (Database Management System)</li>
+					<li>BOOTSTRAP</li>
+					<li>APACHE SERVER</li>
+					<li>PHP</li>
+					<li>MYSQL</li>
+					</ul> <br><br>
+					</p>
+					<a href="about" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div><br><br>
+	<!-- /.row -->
+
+	<center><!-- Portfolio Section -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h3 class="page-header">TECHNOLOGY USED</h3><br><br><br><br> 
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/apache_webserver_logo.png" height="20px" width="100px" alt="">
+                </a>
+		
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/14522157584_a67ab2e898_o.png"height="20px" width="100px" alt="">
+                </a>
+		
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/687474703a2f2f707265636973696f6e2d736f6674776172652e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031342f30342f6a5175726572792e676966.gif" height="20px" width="100px" alt="">
+                </a>
+		
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/Javascript-shopping-cart.png" height="20px" width="100px"alt="">
+                </a>
+		
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/maxresdefault.jpg" height="20px" width="100px"alt="">
+                </a>
+		
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<a href="#">
+                    <img class="img-responsive img-portfolio img-hover" src="images/3271.jpg" height="20px" width="100px"alt="">
+                </a>
+		</center>
+		</div><br><br><br><br>
+	</div>
+	<!-- /.row -->
+
+	<!-- Features Section -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="page-header">&nbsp&nbsp&nbspLEARNONLINE</h2>
+		</div>
+		<div class="col-md-6">
+			<p>&nbsp&nbsp&nbsp&nbspThe LearnOnline by <strong>Jefin Daniel</strong> includes:</p>
+			<ul>
+				<li><strong>Bootstrap v3.3.7</strong>
+				</li>
+				<li>jQuery v1.11.1</li>
+				<li>Font Awesome v4.2.0</li>
+				<li>Working PHP with validation</li>
+				<li>Unstyled page elements for easy customization</li>
+				<li>65 PHP pages</li>
+			</ul>
+			
+		</div>
+		<div class="col-md-6">
+			<img class="img-responsive" src="images/7fb1f193435815a86c8484f82b9589e1.jpg" alt="">
+		</div>
+	</div>
+	
+
+
+  <hr>
+
+    <!-- Contact Us Section -->
+   <div class="row">
+  &nbsp&nbsp      <div class="col-lg-12">
+            <h2 class="page-header">&nbsp&nbspContact Us</h2>
+        </div>
+      &nbsp&nbsp  <div class="col-md-6">
+            <form action="contact_process.php" method="post">
+                <div class="form-group">
+                  &nbsp&nbsp  <label for="name">Name:</label>
+                  &nbsp&nbsp  <input &nbsp&nbsp  type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                 &nbsp&nbsp    <label for="email">Email:</label>
+                 &nbsp&nbsp    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                &nbsp&nbsp     <label for="message">Message:</label>
+                &nbsp&nbsp     <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                </div>
+              &nbsp&nbsp   <button type="submit" class="btn btn-primary">Send Message</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h4>Our Contact Information</h4>
+            <p><strong>Email:</strong> support@learnonline.com</p>
+            <p><strong>Phone:</strong> +1 234 567 890</p>
+            <p><strong>Address:</strong> 123 Learning St, Education City, EC 12345</p>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+
+
+	<?php include('allfoot.php'); ?>
